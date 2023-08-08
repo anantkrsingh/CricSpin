37cc7d1921037d280c0542d35b81b3e952b965a8
anantkrsingh <anantkrplay@gmail.com>
2023-08-08 22:31:14 +0530

    match-odds

--- Frontend/src/Comps/FinishedMatches.jsx
+++ Frontend/src/Comps/FinishedMatches.jsx
@@ -13,6 +13,7 @@ export const FinishedMatches = ({ match }) => {
         console.error('Error parsing JSON:', error.message);
     }
     const item = match
+    console.log(item);
     return (
         // <>
         //     <Link to={{
@@ -60,11 +61,11 @@ export const FinishedMatches = ({ match }) => {
                 <div className='flex w-full justify-between'>
                     <div className='flex flex-col'>
                         <div className='flex flex-row items-center'>
-                            <img src={item.ImageUrl+item.TeamAImage} className='rounded-full border-4  w-[48px] h-[48px]' alt="" />
+                            <img src={item.ImgeURL+item.TeamAImage} className='rounded-full border-4  w-[48px] h-[48px]' alt="" />
                             <h4 className=' euclid font-bold ms-4 text-xl'>{item.TeamA}</h4>
                         </div>
                         <div className='flex flex-row items-center'>
-                            <img src={item.ImageUrl + item.TeamBImage} className='rounded-full border-4  w-[48px] h-[48px]' alt="" />
+                            <img src={item.ImgeURL + item.TeamBImage} className='rounded-full border-4  w-[48px] h-[48px]' alt="" />
                             <h4 className='euclid font-bold ms-4 text-xl'>{item.TeamB}</h4>
                         </div>
                     </div>
