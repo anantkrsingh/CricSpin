3ecd0b1edaa562fefb71a0d2b6ae1342e42913fe
anantkrsingh <anantkrplay@gmail.com>
2023-08-31 23:26:34 +0530

    firebase-changes

--- Frontend/src/Firebase.jsx
+++ Frontend/src/Firebase.jsx
@@ -4,15 +4,14 @@ import { getDatabase } from "firebase/database";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCC0H49d0K67yGYvprPf_5TSrzQG5-j3TE",
-    authDomain: "test-16408.firebaseapp.com",
-    databaseURL: "https://test-16408-default-rtdb.firebaseio.com",
-    projectId: "test-16408",
-    storageBucket: "test-16408.appspot.com",
-    messagingSenderId: "279255768738",
-    appId: "1:279255768738:web:92af83f7e8f7db84fe81fb",
-    measurementId: "G-H4SEF02R65"
-};
+    apiKey: "AIzaSyAmluZ7iPea9IQ_yyK-0NvVj3bsSSfRZX0",
+    authDomain: "cricspin-2183f.firebaseapp.com",
+    projectId: "cricspin-2183f",
+    storageBucket: "cricspin-2183f.appspot.com",
+    messagingSenderId: "1040123982065",
+    appId: "1:1040123982065:web:466cbb95639477d8e07950",
+    measurementId: "G-P4YGS55J19"
+  };
 
 const app = initializeApp(firebaseConfig)
 
