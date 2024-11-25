b4beda03d80a35178e3ac92c88834b5eb0a1ce26
Anant Kumar <115335933+anantkrsingh@users.noreply.github.com>
2024-11-25 20:11:17 +0530

    Update CONST.js

--- Frontend/src/CONST.js
+++ Frontend/src/CONST.js
@@ -1,1 +1,2 @@
-export const IMAGEURL = "https://images.cricspin.live/"
+// export const IMAGEURL = "https://images.cricspin.live/"
+export const IMAGEURL = "https://cricimages.kavachinnovations.com/"
